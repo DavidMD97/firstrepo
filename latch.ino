52c3c5a9788326d69fff4e0d7d05fe9dc652ee71
DavidMD97 <s00173501@mail.itsligo.ie>
2017-02-03 09:18:51 +0000

    Update latch.ino

--- latch.ino
+++ latch.ino
@@ -1,25 +1,13 @@
-/* switch
- * 
- * Each time the input pin goes from LOW to HIGH (e.g. because of a push-button
- * press), the output pin is toggled from LOW to HIGH or HIGH to LOW.  There's
- * a minimum delay between toggles to debounce the circuit (i.e. to ignore
- * noise).  
- *
- * David A. Mellis
- * 21 November 2006
- */
+int inPin = 6;      
+int outPin = 8;      
 
-int inPin = 6;         // the number of the input pin
-int outPin = 8;       // the number of the output pin
+int state = HIGH;      
+int reading;          
+int previous = LOW;    
 
-int state = HIGH;      // the current state of the output pin
-int reading;           // the current reading from the input pin
-int previous = LOW;    // the previous reading from the input pin
 
-// the follow variables are long's because the time, measured in miliseconds,
-// will quickly become a bigger number than can be stored in an int.
-long time = 0;         // the last time the output pin was toggled
-long debounce = 200;   // the debounce time, increase if the output flickers
+long time = 0;         
+long debounce = 200;   
 
 void setup()
 {
@@ -31,9 +19,7 @@ void loop()
 {
   reading = digitalRead(inPin);
 
-  // if the input just went from LOW and HIGH and we've waited long enough
-  // to ignore any noise on the circuit, toggle the output pin and remember
-  // the time
+
   if (reading == HIGH && previous == LOW && millis() - time > debounce) {
     if (state == HIGH)
       state = LOW;
